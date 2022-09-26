46f4c5f3f162e894f3616e8e0e3d08f5b386c126
AriseMarcher <2543061999@qq.com>
2022-09-26 09:04:29 +0800

    路由导航

--- pages/about.vue
+++ pages/about.vue
@@ -3,11 +3,28 @@
     <h1>
       Hello about.vue
     </h1>
+    <!-- a链接，刷新导航，走服务端渲染 -->
+    <h2>a 链接</h2>
+    <a href="/">跳转到首页</a>
+
+    <!-- router-link 导航链接组件 -->
+    <h2>router-link</h2>
+    <router-link to="/">去首页</router-link>
+
+    <!-- 编程式导航 -->
+    <h2>编程式导航</h2>
+    <button @click="gotoHome">去首页</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'About'
+  name: 'About',
+  methods: {
+    gotoHome () {
+      console.log('hello')
+      this.$router.push('/')
+    }
+  }
 }
 </script>
