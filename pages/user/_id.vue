fb6cd64533759136ed2d1eeda757a22eb4f46e45
AriseMarcher <2543061999@qq.com>
2022-09-26 09:29:57 +0800

    动态路由

---
+++ pages/user/_id.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>
+      UserPage
+      <p>this.$route.params.id</p>
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserPage',
+
+  mounted() {
+    console.log(this.$route)
+  }
+}
+</script>
